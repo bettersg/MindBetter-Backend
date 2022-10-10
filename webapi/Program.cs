0404a4e9985bb5239d71f1dd55e188c826a9f789
karthik-Jay <karthik.jay@outlook.com>
2022-10-10 20:34:19 +0800

    Untracked binary folders
    1. Modified .gitignore to untrack .vs folder.
    2. Untracked obj folder
    3. Untracked .vs folder

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -1,4 +1,15 @@
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:3000");
+                      });
+});
 
 // Add services to the container.
 
@@ -17,6 +28,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
