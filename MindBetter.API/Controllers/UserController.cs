db55e12b635b48cf03916b026c883a49a5398ce8
karthik-Jay <karthik.jay@outlook.com>
2022-10-16 14:34:56 +0800

    Renamed Web project to API and created test get API for users type.

---
+++ MindBetter.API/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using MindBetter.Core.Entities;
+using MindBetter.Infrastructure.Data;
+
+namespace MindBetter.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly ILogger<UserController> _logger;
+    private AppDbContext _appDbContext;
+
+    public UserController(ILogger<UserController> logger, AppDbContext appDbContext)
+    {
+        this._logger = logger;
+        this._appDbContext = appDbContext;
+    }
+
+    [HttpGet(Name = "GetUsers")]
+    public IEnumerable<User> Get()
+    {
+        return _appDbContext.Users.ToList();
+    }
+}
--- MindBetter.API/Program.cs
+++ MindBetter.API/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MindBetter.Infrastructure.Data;
+
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.AddConsole();
 
 builder.Services.AddCors(options =>
 {
@@ -18,7 +24,27 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("MindBetter.TestDb"));
+
 var app = builder.Build();
+
+app.Logger.LogInformation("App created...");
+
+app.Logger.LogInformation("Seeding Database...");
+
+using (var scope = app.Services.CreateScope())
+{
+    var scopedProvider = scope.ServiceProvider;
+    try
+    {
+        var appDbContext = scopedProvider.GetRequiredService<AppDbContext>();
+        await AppDbContextSeed.SeedAsync(appDbContext, app.Logger);
+    }
+    catch (Exception ex)
+    {
+        app.Logger.LogError(ex, "An error occurred seeding the DB.");
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
