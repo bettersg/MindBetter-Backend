2e08a00fbde11265453fa43fdfede21ed7544660
karthik-Jay <karthik.jay@outlook.com>
2022-10-16 14:34:09 +0800

    Added unit tests project
    Dependent on xunit framework.

---
+++ UnitTests/Core/Entities/EnumBaseEntityTests.cs
@@ -0,0 +1,37 @@
+﻿using MindBetter.Core.Entities;
+using MindBetter.Core.Entities.NPMHOAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTests.Core.Entities
+{
+    public class EnumBaseEntityTests
+    {
+        [Fact]
+        public void CompareSameClasses_ReturnTrue()
+        {
+            Assert.Equal(new UserType(UserTypeEnum.Admin), new UserType(UserTypeEnum.Admin));
+        }
+
+        [Fact]
+        public void CompareSameClasses_ReturnFalse()
+        {
+            Assert.NotEqual(new UserType(UserTypeEnum.Admin), new UserType(UserTypeEnum.Member));
+        }
+
+        [Fact]
+        public void CompareDifferentDerivedClasses_ReturnFalse()
+        {
+            EnumBaseEntity<UserTypeEnum> ut = new UserType(UserTypeEnum.Admin);
+            EnumBaseEntity<ServiceCategoryEnum> sc = new ServiceCategory(ServiceCategoryEnum.Cat1);
+
+            var result = ut.Equals(sc);
+            
+            Assert.False(result);
+        }
+    }
+}
---
+++ UnitTests/Infrastructure/Seed.cs
@@ -0,0 +1,26 @@
+﻿using MindBetter.Core.Entities;
+using MindBetter.Core.Entities.NPMHOAggregate;
+using MindBetter.Infrastructure.Data;
+using Xunit;
+
+namespace UnitTests.Infrastructure
+{
+    public class Seed
+    {
+        [Fact]
+        public void Generate_EnumLookupTable_ReturnListOfEnum()
+        {
+            var expected = new List<UserType>
+            {
+                new UserType(UserTypeEnum.Admin),
+                new UserType(UserTypeEnum.Editor),
+                new UserType(UserTypeEnum.Member)
+            };
+
+            var actual = AppDbContextSeed.GetEnumLookupTable<UserType, UserTypeEnum>().ToList();
+
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
